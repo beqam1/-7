05256b7f3de2d9e339a9ed5d1ae3a4146d44b258
Mona Lisa <b.meparishvili123@gmail.com>
2025-11-26 19:16:23 +0400

    davaleba

---
+++ davaleba8.js
@@ -0,0 +1,48 @@
+let a = 15
+let b = 20
+console.log(a + b)
+
+
+let student = {
+    firstname: "beqa",
+    lastname: "meparishvili",
+    age: 23,
+    email: "beqamepara@gmail.com",
+    marks: [85, 90, 78]
+}
+console.log(student.firstname);
+
+
+let m = 45;
+let n = 23;
+if (m > n) {
+    console.log(m + n);
+} else if (n < m) {
+    console.log(n);
+}
+
+
+let studentname = "beqa";
+console.log("rise and shine, " + studentname + " ! ");
+
+
+let bonus;
+let salary = 1500;
+if (salary >= 2000) {
+    bonus = 0;
+} else {
+    bonus = salary * 0.1;
+}
+console.log(bonus);
+
+
+/*
+გამოაცხადეთ ცვლადი bonus და salary ცვლადები, salary -ს მიანიჭეთ სასურველი რიცხვითი მნიშვნელობა,
+დაწერეთ თუ ხელფასი არის 2000 ლარი ან მეტი მაშინ ბონუსი არის 0, თუ ხელფასი 2000 ze ნაკლებია მაშინ ბონუსი არის ხელფასის 10%. 
+დაბეჭდეთ ბონუსი (ჯერ bonus ცვლადს უნდა მიანიჭოთ შესაბამისი მნიშვნელობა და შემდეგ დაბეჭდოთ bonus)
+*/
+
+
+
+
+
